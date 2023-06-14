75437dcd4cd177bddc0d1fe7ba4967b7e5662cb2
Ubuntu <ubuntu@ip-172-31-6-93.ap-south-1.compute.internal>
2023-06-14 11:46:29 +0000

    Hello World

---
+++ 02/myscript.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo Script will now run
+echo "The shell we are using is $SHELL"
+echo Script will now end... bye
+echo By the way, Hello World!!!
+echo and my current working directory is:
+pwd
